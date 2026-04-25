35e38cec6c4924894d4095c4973ba4161ceb4ed9
HASANCoU <mehedi.hasancou2@gmail.com>
2026-04-25 22:12:55 +0600

    History Context API Added

---
+++ src/app/context/GlobalContextProvider.jsx
@@ -0,0 +1,19 @@
+'use client';
+import React, { createContext, useContext, useState } from 'react';
+
+const GlobalContext = createContext();
+
+const GlobalContextProvider = ({ children }) => {
+  const [logInfo, setLogInfo] = useState([]);
+
+  return (
+    <GlobalContext.Provider value={{ logInfo, setLogInfo }}>
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+// custom hook (important)
+export const useGlobalContext = () => useContext(GlobalContext);
+
+export default GlobalContextProvider;
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
+import GlobalContextProvider from "./context/GlobalContextProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,9 @@ export default function RootLayout({ children }) {
     >
       <body className="min-h-full flex flex-col">
         <NavBar/>
-        {children}
+        <GlobalContextProvider>
+          {children}
+        </GlobalContextProvider>
         <Footer/>
         </body>
     </html>
